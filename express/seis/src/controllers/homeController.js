23929f4e8b821346ad4cc1d663c4e911d7e008d4
João Marcos Cerqueira <joaombcerqueira@gmail.com>
2022-02-03 21:43:56 -0300

    mongoose tests

--- express/seis/src/controllers/homeController.js
+++ express/seis/src/controllers/homeController.js
@@ -1,3 +1,16 @@
+// const HomeModel = require('../models/HomeModel')
+
+/**async function criaTeste() {
+    try {
+        const  dados = await HomeModel.find()
+        console.log(dados)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+criaTeste()**/
+
 exports.paginaInicial = (req, res) => {
     res.render('index')
     return // finalizada a req
